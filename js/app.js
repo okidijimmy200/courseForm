c6f7a6252ec498e64ce0e90ae46bb0f6de2eb9e0
okidijimmy200 <okidijimmie@gmail.com>
2020-10-19 07:31:55 +0300

    completed courseForm project

--- js/app.js
+++ js/app.js
@@ -9,6 +9,25 @@
         display.hideSubmit();
         
     })
+    //what to do incase we r submitting the form(add customer on submit)
+document.getElementById('customer-form').addEventListener('submit', function(event){
+    //prevent default
+    event.preventDefault();
+    //grab values accesible to you
+    const name = this.querySelector('.name')
+    const course = this.querySelector('.course')
+    const author = this.querySelector('.author')
+
+    //NB: this keyword points to the parent document ie customer-form in this case
+
+    //create new customer instance
+    const customer = new Customer(name.value, course.value, author.value)
+    const display = new Display();
+
+    //add 2 methods to display prototypes
+    display.feedback(customer);
+    display.clearFields();
+});
     //1)constructos reponsible for display
     function Display(){
         //pt to instnce created
@@ -51,5 +70,76 @@
         const btn = document.querySelector('.submitBtn')
         //display property
         btn.disabled = true;
+    }
+//show loading and feedback(we use keyword for a method in a func)
+Display.prototype.feedback = function(customer){
+    const feedback = document.querySelector('.feedback');
+    const loading = document.querySelector('.loading');
+
+    //show item, alert and alert success
+    feedback.classList.add('showItem', 'alert', 'alert-success');
+    loading.classList.add('showItem')
+// hide the submiy btn when we click the form nd its loading
+    const self = this;
+    self.hideSubmit()
+
+//setTimeout
+setTimeout(function(){
+    //remove feedback and loading
+    feedback.classList.remove('showItem', 'alert', 'alert-success')
+    loading.classList.remove('showItem');
+    self.addCustomer(customer)
+},4000)
+}
+
+//method to display customer
+Display.prototype.addCustomer = function(customer){
+    // console.log(customer)
+    //creating random image
+    const random = this.getRandom()
+    const div = document.createElement('div');
+    div.classList.add('col-11', 'mx-auto', 'col-md-6', 'my-3', 'col-lg-4');
+    div.innerHTML= `<div class="card text-left">
+    <img src="img/cust-${random}.jpg" class="card-img-top" alt="">
+    <div class="card-body">
+     <!-- customer name -->
+     <h6 class="text-capitalize "><span class="badge badge-warning mr-2">name :</span><span id="customer-name"> ${customer.name}</span></h6>
+     <!-- end of customer name -->
+     <!-- customer name -->
+     <h6 class="text-capitalize my-3"><span class="badge badge-success mr-2">course :</span><span id="customer-course">
+     ${customer.course}
+      </span></h6>
+     <!-- end of customer name -->
+     <!-- customer name -->
+     <h6 class="text-capitalize"><span class="badge badge-danger mr-2">author :</span><span id="course-author"> ${customer.author}</span></h6>
+     <!-- end of customer name -->
+    </div>
+   </div>
+`;
+//appendChild
+this.customers.appendChild(div);
+};
+//random number
+Display.prototype.getRandom = function(){
+    let random = Math.floor(Math.random()*5+1)
+    return random;
+}
+
+//displayPrototype of clearFields
+Display.prototype.clearFields = function(){
+    this.name.value = '';
+    this.course.value = '';
+    this.author.value = '';
+
+    //remove the complete and remove classes
+    this.name.classList.remove('complete', 'fail');
+    this.course.classList.remove('complete', 'fail');
+    this.author.classList.remove('complete', 'fail');
+}
+    //customer constructorfunc is responsible for all the instances of customers we r creating
+    function Customer(name, course, author){
+        this.name = name;
+        this.course = course;
+        this.author = author;
     }
 })();
